2f66c404912ab68a6c0be29627e899678f3de7d5
phongvt3110 <vtphong651043@gmail.com>
2018-09-28 00:44:18 +0700

    update code

--- app.py
+++ app.py
@@ -3,11 +3,15 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return render_template('index.html')
+    return render_template('home/index.html')
 
 @app.route("/home")
 def home():
     return render_template('home/index.html')
+
+@app.route("/admin")
+def admin():
+    return render_template('admin/index.html')
 
 if __name__ == "__main__":
     app.run()
