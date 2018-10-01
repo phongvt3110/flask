73a52e271de752c98e82118e322d4cae2c479574
ex-phongvt <ex.phongvt@nextbeat.net>
2018-10-01 11:49:11 +0700

    update code

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, json
 app = Flask(__name__)
 
 @app.route("/")
@@ -11,6 +11,19 @@ def home():
 
 @app.route("/admin")
 def admin():
+    return render_template('admin/index.html')
+
+@app.route("/signUp", methods=['POST'])
+def signup():
+    _name = request.form['inputName']
+    _email = request.form['inputEmail']
+    _password = request.form['inputPassword']
+    _address = request.form['inputAddress']
+    _phone  = request.form['inputPhone']
+    if _name and _email and _password:
+        return json.dump({'html':'<span>All fields good !!</span>'})
+    else:
+        return json.dump({'html':'<span>Enter the required fields</span>'})
     return render_template('admin/index.html')
 
 if __name__ == "__main__":
